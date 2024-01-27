6e180580bdabd11b12bedb2964d9f8ef323bb3a1
silvereengames <silvereennetwork@gmail.com>
2024-01-27 03:35:24 -0500

    upload the thing

---
+++ src/App.jsx
@@ -0,0 +1,120 @@
+import "./App.css";
+
+const games = [
+  {
+    name: "Ball Dodge",
+    image: "https://moonlight.silvereen.net/src/static/icons/ball-dodge.png",
+    link: "https://moonlight.silvereen.net/src/static/games/ball-dodge/index.html",
+    description: "A simple game where you have to dodge the balls.",
+    download:
+      "https://moonlight.silvereen.net/src/static/games/ball-dodge/ball-dodge.zip",
+  },
+];
+
+const sites = [
+  {
+    name: "Moonlight",
+    image:
+      "https://moonlight.silvereen.net/src/static/images/moon-light-moon-background.png",
+    link: "https://moonlight.silvereen.net",
+    description: "The cure to boredom at your fingertips",
+    download: "",
+  },
+  {
+    name: "Corrupted",
+    image: "https://corruptedgaming.online/corrupted-logo.png",
+    link: "https://corruptedgaming.online",
+    description: "Play all the retros in one place",
+    download: "https://github.com/silvereennetwork/corrupted/",
+  },
+];
+
+function Spacer15(pix) {
+  return <div style={{ width: "1px", height: "15vh" }}></div>;
+}
+
+function Spacer5(pix) {
+  return <div style={{ width: "1px", height: "5vh" }}></div>;
+}
+
+function Games() {
+  const gamesObject = games.map((games) => (
+    <a href={games.link} target="_new">
+      <div class="object-div">
+        <img src={games.image} alt={games.name} class="object-img"></img>
+        <p class="object-title">{games.name}</p>
+        <p class="object-description">{games.description}</p>
+        <a href={games.download} target="_new" download>
+          <span
+            class="material-symbols-outlined"
+            style={{ backgroundColor: "#1a1a1a", display: "block" }}
+          >
+            download
+          </span>
+        </a>
+      </div>
+    </a>
+  ));
+
+  return gamesObject;
+}
+
+function Sites() {
+  const sitesObject = sites.map((sites) => {
+    if (sites.download !== "") {
+      return (
+        <a href={sites.link} target="_new">
+          <div class="object-div">
+            <img src={sites.image} alt={sites.name} class="object-img"></img>
+            <p class="object-title">{sites.name}</p>
+            <p class="object-description">{sites.description}</p>
+            <a href={sites.download} target="_new" download>
+              <span
+                class="material-symbols-outlined"
+                style={{ backgroundColor: "#1a1a1a", display: "block" }}
+              >
+                download
+              </span>
+            </a>
+          </div>
+        </a>
+      );
+    } else {
+      return (
+        <a href={sites.link} target="_new">
+          <div class="object-div">
+            <img src={sites.image} alt={sites.name} class="object-img"></img>
+            <p class="object-title">{sites.name}</p>
+            <p class="object-description">{sites.description}</p>
+          </div>
+        </a>
+      );
+    }
+  });
+
+  return sitesObject;
+}
+
+export default function App() {
+  return (
+    <>
+      <h1 class="center title">
+        Silvereen Games <span style={{ color: "lime" }}>Archive</span>
+      </h1>
+      <Spacer15 />
+      <h2 class="center">Games</h2>
+      <hr></hr>
+      <Spacer5 />
+      <div class="objects-container">
+        <Games />
+      </div>
+      <Spacer15 />
+      <h2 class="center">Sites</h2>
+      <hr />
+      <Spacer5 />
+      <div class="objects-container">
+        <Sites />
+      </div>
+    </>
+  );
+}
---
+++ src/projects/projects.jsx
@@ -0,0 +1,8 @@
+const games = [
+  {
+    name: "Ball Dodge",
+    image: "https://moonlight.silvereen.net/src/static/icons/ball-dodge.png",
+    link: "https://moonlight.silvereen.net/src/static/games/ball-dodge/index.html",
+    description: "A simple game where you have to dodge the balls.",
+  },
+];
